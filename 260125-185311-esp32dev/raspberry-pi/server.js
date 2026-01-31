ad5d3adbf7a7e0ff67c314ad674f68dad4d20a36
nkepah <ngomapa26@gmail.com>
2026-01-30 20:02:56 -0600

    Auto commit

--- 260125-185311-esp32dev/raspberry-pi/server.js
+++ 260125-185311-esp32dev/raspberry-pi/server.js
@@ -168,7 +168,7 @@ setInterval(updateAllDeviceStatus, 30000);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dashboard')));
 
 // =============================================================================
 // API ROUTES
